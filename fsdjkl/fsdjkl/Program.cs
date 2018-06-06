56cb847badfa5e5a326801143031e923ea7ff8d4
Reena <19yuanr@elmbrookstudents.org>
2018-06-06 11:02:09 -0500

    INITIALIZING DONG

---
+++ fsdjkl/fsdjkl/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace fsdjkl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
